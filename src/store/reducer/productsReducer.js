ea496506bf746ff3e1856a49836f01accdab9e2e
ananda <anandamohanram@gmail.com>
2019-06-27 03:36:12 +1200

    create reducers templates

---
+++ src/store/reducer/productsReducer.js
@@ -0,0 +1,22 @@
+const initialState = [
+    {
+        name: "Sledgehammer",
+        price: 125.76
+    }, {
+        name: "Axe",
+        price: 190.51
+    }, {
+        name: "Bandsaw",
+        price: 562.14
+    }, {
+        name: "Chisel",
+        price: 13.9
+    }, {
+        name: "Hacksaw",
+        price: 19.45
+    }
+]
+
+export default (state = initialState, { type, payload }) => {
+    return state
+}
---
+++ src/store/reducer/shoppingCartReducer.js
@@ -0,0 +1,9 @@
+const initialState = { products: [], total: 0 }
+
+export default (state = initialState, { type, payload }) => {
+    switch (type) {
+
+    default:
+        return state
+    }
+}
