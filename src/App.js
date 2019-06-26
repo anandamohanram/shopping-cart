a84f6f06041c0cef3392c2cb135345bf5e81eee6
ananda <anandamohanram@gmail.com>
2019-06-27 03:45:54 +1200

    add provider to app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import {Provider} from 'react-redux';
+import { NavBar } from './component';
 import './App.css';
 import Routes from './router/Routes';
-import { BrowserRouter } from 'react-router-dom'
-import { NavBar } from './component';
+import store from './store/store'
 
 function App() {
   return (
 
+    <Provider store={store}>
     <BrowserRouter>
       <div className="App">
         <NavBar />
         <Routes />
       </div>
-
     </BrowserRouter>
+    </Provider>
   );
 }
 
