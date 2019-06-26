1aad5563023be39d13d0f8c471cf06de44ab7eaa
ananda <anandamohanram@gmail.com>
2019-06-27 03:05:02 +1200

    add routes file

---
+++ src/router/Routes.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import {Switch, Route} from 'react-router-dom'
+import {ProductList, ShoppingCart} from '../container'
+
+
+export class Routes extends Component {
+    render() {
+        return (
+                <Switch>
+                    <Route exact path='/' component={ProductList} />
+                    <Route exact path='/cart' component={ShoppingCart} />
+                </Switch>
+        )
+    }
+}
+
+export default Routes
