3af1376fa7bee00db74a160b4a92d44a29ff7dda
ananda <anandamohanram@gmail.com>
2019-06-27 03:09:13 +1200

    add Navbar and components index file

---
+++ src/component/navBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './NavBar.module.css'
+import {NavLink} from 'react-router-dom'
+
+function NavBar() {
+    return (
+        <div className="navBarContainer">
+            <NavLink className={styles.navButton} activeClassName={styles.activeNavButton} exact to="/">Products</NavLink>
+            <NavLink className={styles.navButton} activeClassName={styles.activeNavButton} to="/cart">Cart</NavLink>
+        </div>
+    )
+}
+
+export default NavBar
