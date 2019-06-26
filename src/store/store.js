334f583d0197550ffcea49455e1859325edef769
ananda <anandamohanram@gmail.com>
2019-06-27 04:54:11 +1200

    save cart to local storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,23 @@
 import { combineReducers , createStore } from 'redux';
 import {productsReducer, shoppingCartReducer} from './reducer'
+import {saveCart, hydrateCart} from './localStorage'
 
 const reducers = combineReducers({productsReducer, shoppingCartReducer});
 
+const localCart = hydrateCart();
+
 const store = createStore(
                 reducers, 
+                localCart,
                 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+
+store.subscribe(() => {
+    saveCart({
+        shoppingCartReducer: store.getState().shoppingCartReducer
+  });
+});
+
 
 export default store;
