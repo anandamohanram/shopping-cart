3d4f40cd42e9e9ab2e1b675e2ef77fe76c522ba6
ananda <anandamohanram@gmail.com>
2019-06-27 02:54:44 +1200

    Make ProductList/ShoppingCart container outlines

---
+++ src/container/shoppingCart/ShoppingCart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ShoppingCart extends Component {
+    render() {
+        return (
+            <div>
+                Shopping Cart
+            </div>
+        )
+    }
+}
+
+export default ShoppingCart
